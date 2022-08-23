a6038c920c8e49e85a85600e07f5898b4faa24ed
Aaroh1 <aarohsjcian1@gmail.com>
2022-08-23 22:56:00 +0530

    Basic Setup of Frontend (UI overhauls and some routing left)

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,43 +1,64 @@
 import React, { useEffect, useState } from 'react'
 import './Dashboard.css';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation ,useParams} from 'react-router-dom';
 import axios from 'axios';
 import { CCard, CRow, CCol, CCardImage, CCardBody, CCardTitle, CCardText } from '@coreui/react'
-import Button from '@mui/material/Button';
+import Button from 'react-bootstrap/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 export default function Dashboard() {
+  const para=useParams();
   const [userItems, setuserItems] = useState([]);
   const search = useLocation().search;
   const qparams = new URLSearchParams(search);
   const imgpath='http://localhost:3001/public/img/';
+  const Navigate=useNavigate();
+  async function logout()
+  {
+    await axios({
+      method:"GET",
+      withCredentials: true,
+      url: "http://localhost:3001/Logout",
+    });
+    Navigate("/",{replace:true});
+  }
+  async function Delete(it)
+  {
+    console.log(it);
+    await axios(
+      {
+        method:"DELETE",
+        data:{email:qparams.get("q"),item:it._id},
+        withCredentials: true,
+        url: "http://localhost:3001/deleteitem",
+      }
+    ).then((da)=>console.log(da));
+  }
   useEffect(() => {
     axios({
       mehod: "GET",
       withCredentials: true,
       url: "http://localhost:3001/Dashboard",
       params: { email: qparams.get("q") }
-    }).then((data) => setuserItems(data.data));
+    }).then((data) => {setuserItems(data.data);});
   })
   function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("mySidebar").style.opacity = "90%";
-    document.querySelector(".RightSide").style.marginLeft = "250px";
+    document.querySelector(".RightSide").style.opacity = "80%";
   }
 
   function closeNav() {
     document.getElementById("mySidebar").style.width = "70px";
-    document.getElementById("mySidebar").style.opacity = "100%";
-    document.querySelector(".RightSide").style.marginLeft = "70px";
+    document.querySelector(".RightSide").style.opacity = "100%";
   }
   const ItemCard = (data) => {
-    return (<div className='Card'>
+    return (<div className='Card' >
       <CCard className="mb-3 border-dark" style={{ width: '440px', height:"200px"}}>
         <CRow className="g-0">
           <CCol md={4}>
             <CCardImage src={imgpath+data.item} style={{ width:"180px", height:"200px"}}/>
           </CCol>
           <CCol md={6}>
-            <CCardBody>
+            <CCardBody style={{marginLeft:"40px",marginTop:"-10px"}}>
               <CCardTitle>{data.type}</CCardTitle>
               <CCardText>
                 {data.desc}
@@ -45,11 +66,9 @@ export default function Dashboard() {
               <CCardText>
                 <small className="text-medium-emphasis">Found on {data.date.slice(0,10)} in {data.location}</small>
               </CCardText>
-            </CCardBody>
-          </CCol>
-          <Button variant="outlined" startIcon={<DeleteIcon />}>
-            Delete
-          </Button>
+              <Button variant="outline-danger" onClick={()=>Delete(data)}><img src="https://img.icons8.com/color/48/000000/delete-forever.png" style={{height:"25px",width:"25px"}}/></Button>{' '}
+            </CCardBody>  
+          </CCol>  
         </CRow>
       </CCard>
     </div>)
@@ -61,9 +80,9 @@ export default function Dashboard() {
 
   return (
 
-    <div>
+    <div className='ParentDiv'>
       <div id="mySidebar" className="sidebar" onMouseOver={openNav} onMouseLeave={closeNav} >
-        <a href="#"><img className='i' src="https://img.icons8.com/ios/50/000000/home--v1.png" />HOME</a>
+        <a href={`/Home?q=${qparams.get('q')}`}><img className='i' src="https://img.icons8.com/ios/50/000000/home--v1.png" />HOME</a>
         <a href={`/Upload?q=${qparams.get('q')}`}><img className='i' src="https://img.icons8.com/external-those-icons-lineal-those-icons/100/000000/external-Add-notes-those-icons-lineal-those-icons.png" />ADD ITEM</a>
         <a href="#"><img className='i' src="https://img.icons8.com/ios-glyphs/90/000000/person-male.png" />USER PAGE</a>
       </div>
@@ -71,8 +90,7 @@ export default function Dashboard() {
       <div className='RightSide' >
         <div className="HorizontalNav">
           <h2>Collapsed Sidebar</h2>
-        </div>
-
+          <Button variant="secondary" onClick={logout}>Logout</Button>{' '}        </div>
         <div className='DisplayUserItems'> {DisplayItemCards()} </div>
 
       </div>
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -4,50 +4,70 @@ import { useState } from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { useNavigate,useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Button from 'react-bootstrap/esm/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
 export default function MainPage() {
-    const search=useLocation().search;
-    const qparams=new URLSearchParams(search);
-    const [allItems,setallItems]=useState([]);
-    const imgpath='http://localhost:3001/public/img/';
-    const Navigate=useNavigate();
-    useEffect(()=>{
-      axios({
-        method:"get",
-        withCredentials: true,
-        url:"http://localhost:3001/Home",
-      }).then((data)=>{
-        console.log("HERE")
-        if(data.status===200)
-        Navigate(data.data,{replace:true});
-      })
-    },[])
-    useEffect(()=>{
-        axios({
-            method:"get",
-            withCredentials: true,
-            url:"http://localhost:3001/getAllItems",
-              }).then((data)=>{
-                console.log(data.data);
-                setallItems(data.data);
-                // console.log(allItems);
-              });
-    },[])
-    function DisplayItemCards()
-    {
-      return(allItems?.map((it)=>{return ItemCard(it)}));
+  const search = useLocation().search;
+  const qparams = new URLSearchParams(search);
+  const [allItems, setallItems] = useState([]);
+  const [searchValue, setsearch] = useState('');
+  const imgpath = 'http://localhost:3001/public/img/';
+  const Navigate = useNavigate();
+  let itemarray=allItems
+  function handleSearchChange() {
+    if (!searchValue.length) { return DisplayItemCards(itemarray); }
+    else {
+      return DisplayItemCards(itemarray.filter((p) => {
+        if (((p.type).toLowerCase()).includes(searchValue.toLowerCase()))
+          return p
+      }))
     }
-    const ItemCard=(itemdata)=>{
-    return(<Card style={{ width: '20rem', border:"black 1px solid"}}>
-      <Card.Img variant="top" src={imgpath+itemdata.item} style={{ height: '12rem'}} />
+  }
+  async function logout() {
+    await axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:3001/Logout",
+    });
+    Navigate("/", { replace: true });
+  }
+  useEffect(() => {
+    axios({
+      method: "get",
+      withCredentials: true,
+      url: "http://localhost:3001/Home",
+    }).then((data) => {
+      console.log("HERE")
+      if (data.status === 200)
+        Navigate(data.data, { replace: true });
+    })
+  }, [])
+  useEffect(() => {
+    axios({
+      method: "get",
+      withCredentials: true,
+      url: "http://localhost:3001/getAllItems",
+    }).then((data) => {
+      console.log(data.data);
+      setallItems(data.data);
+      // console.log(allItems);
+    });
+  }, [])
+  function DisplayItemCards(filtereditems) {
+    return (filtereditems?.map((it) => { return ItemCard(it) }));
+  }
+  const ItemCard = (itemdata) => {
+    return (<Card style={{ width: '20rem', border: "black 1px solid", marginBottom: "7px" }}>
+      <Card.Img variant="top" src={imgpath + itemdata.item} style={{ height: '12rem' }} />
       <Card.Body>
         <Card.Title>{itemdata.type}</Card.Title>
         <Card.Text>
-        {itemdata.desc}
+          {itemdata.desc}
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Found On: {itemdata.date.slice(0,10)}</ListGroup.Item>
+        <ListGroup.Item>Found On: {itemdata.date.slice(0, 10)}</ListGroup.Item>
         <ListGroup.Item>Found in: {itemdata.location}</ListGroup.Item>
       </ListGroup>
       <Card.Body>
@@ -55,35 +75,64 @@ export default function MainPage() {
         <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>)
-    }
-    function openNav() {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("mySidebar").style.opacity ="90%";
-        document.querySelector(".RightSide").style.marginLeft = "250px";
-      }
-      
-      function closeNav() {
-        document.getElementById("mySidebar").style.width = "70px";
-        document.getElementById("mySidebar").style.opacity ="100%";
-        document.querySelector(".RightSide").style.marginLeft = "70px";
-      }
-    return (
-        <div>
-            <div id="mySidebar" className="sidebar"  onMouseOver={openNav} onMouseLeave={closeNav} >
-            <a href="#"><img className='i' src="https://img.icons8.com/ios/50/000000/home--v1.png"/>HOME</a>
-                <a href={`/Upload?q=${qparams.get('q')}`}><img className='i'   src="https://img.icons8.com/external-those-icons-lineal-those-icons/100/000000/external-Add-notes-those-icons-lineal-those-icons.png"/>ADD ITEM</a>
-                <a href={`/Dashboard?q=${qparams.get("q")}`}><img className='i' src="https://img.icons8.com/ios-glyphs/90/000000/person-male.png"/>USER PAGE</a>
-            </div>
+  }
+  function sortDef(){
+    itemarray=allItems}
+  function sortDate(){
+    console.log("SortDate")
+    itemarray.sort((a, b) => (a.date > b.date) ? 1 : -1)
+    console.log(itemarray);
+  }
+  function sortLoc(){
+    console.log("SortLoc")
+    itemarray.sort((a, b) => (a.location > b.location) ? 1 : -1)
+    console.log(itemarray);
+  }
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.querySelector(".RightSide").style.opacity = "80%";
+  }
 
-            <div className='RightSide' >
-                <div className="HorizontalNav">
-                <h2>Collapsed Sidebar</h2>
-                </div>
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "70px";
+    document.querySelector(".RightSide").style.opacity = "100%";
+  }
+  return (
+    <div>
+      <div id="mySidebar" className="sidebar" onMouseOver={openNav} onMouseLeave={closeNav} >
+        <a href={`/Home?q=${qparams.get('q')}`}><img className='i' src="https://img.icons8.com/ios/50/000000/home--v1.png" />HOME</a>
+        <a href={`/Upload?q=${qparams.get('q')}`}><img className='i' src="https://img.icons8.com/external-those-icons-lineal-those-icons/100/000000/external-Add-notes-those-icons-lineal-those-icons.png" />ADD ITEM</a>
+        <a href={`/Dashboard?q=${qparams.get("q")}`}><img className='i' src="https://img.icons8.com/ios-glyphs/90/000000/person-male.png" />USER PAGE</a>
+      </div>
 
-            <div className='DisplayItems'>{DisplayItemCards()}</div>
-            
-            </div>
-            
+      <div className='RightSide' >
+        <div className="HorizontalNav">
+          <h2>Collapsed Sidebar</h2>
+          <Button variant="secondary" onClick={logout}>Logout</Button>{' '}
+
         </div>
-    )
+
+        <div className='DisplayItems'>
+          <div className='Filtering' style={{ width: "100%", marginBottom: "5%" }}>
+            <div className='SearchBorder'>
+              <input className='Search Bar' value={searchValue} placeholder="Type Item Name...." onChange={(e) => { setsearch(e.target.value); }} style={{ width: "350px", height: "50px", borderRadius: "50px", padding: "20px" }} />
+            </div>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Sort By
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1" onClick={sortDate}>Date</Dropdown.Item>
+                <Dropdown.Item href="#/action-2" onClick={sortLoc}>Location</Dropdown.Item>
+               </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          {handleSearchChange()}
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
